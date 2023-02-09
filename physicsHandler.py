c44c997d2a57402a23442b41709cd7f7b963ef79
bax2004rj <bax2004rj@gmail.com>
2023-02-09 15:10:27 -0500

    Started using box2d It is very slow and uses tons of ram, next thing is to make it destroy objects

--- physicsHandler.py
+++ physicsHandler.py
@@ -1,4 +1,10 @@
 import math
+
+import Box2D
+from Box2D.b2 import (world, polygonShape, circleShape, staticBody, dynamicBody)
+
+ppm = 224.650948589
+
 def fire(botHeldDisks,discX,discY,targetX,targetY,targetI,targetXInv,targetYInv,botX,botY,botDir,power,angle): # Solve firing physics
     if botHeldDisks>0:
         powerRange = ((power**2)*(math.sin(2*angle)))/-9.81
@@ -17,4 +23,43 @@ def fire(botHeldDisks,discX,discY,targetX,targetY,targetI,targetXInv,targetYInv,
         elif targetY[-1] < botY:
             targetYInv.append(False)
         print("Range: %f,Target pos:(%d,%d)"%(powerRange,targetX[-1],targetY[-1]))
+
+world = world(gravity=(0, -10), doSleep=True)
+
+ground_body = world.CreateStaticBody(
+    position=(0, 0),
+    shapes=polygonShape(box=(800, 800)),
+)
+
+def updatePhysics(discX,discY,targetI,botx,boty,botdir,fps): # Create dynamic bodies for box2d
+    global world
+    newdiscX = []
+    newdiscY = [] 
+    newdiscI = []
+    body = []
+    circle = []
+    bot = world.CreateDynamicBody(position = (botx/ppm,boty/ppm))
+    botPhysicRect = bot.CreatePolygonFixture(box = (0.5,0.5))
+    for i in range(len(discX)):# Eliminate any discs currently being animated
+        if not i in targetI:
+            newdiscX.append(discX[i])
+            newdiscY.append(discY[i])
+            newdiscI.append(i)
+    for i in range(len(newdiscX)): # Give it positions
+        posX = newdiscX[i]/ppm
+        posY = newdiscY[i]/ppm
+        body.append(world.CreateDynamicBody(position=(posX,posY)))
+        circle.append(body[-1].CreateCircleFixture(radius=0.07, density=1, friction=0.3))
+    try:    
+        world.Step(1/fps,10,10) # Give it time
+    except ZeroDivisionError:
+        world.Step(1,10,10)
+    # Steal those positions back!
+    try:
+        for i in newdiscI:
+            discX[newdiscI.index(i)] = newdiscX[i]
+            discY[newdiscI.index(i)] = newdiscY[i]
+    except IndexError:
+        pass
+    return discX,discY
 
