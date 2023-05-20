de92ed3560abb5cf642ce1c146cd97b997d83f30
bax2004rj <bax2004rj@gmail.com>
2023-05-19 23:48:19 -0400

    Delete and restart

--- updater.py
+++
@@ -1,5 +0,0 @@
-import uiHandler
-import fileHandler
-class updateWin():
-    def __init__(self,screen,win,events):
-        uiHandler.draw_text(screen,win.x,win.y,fileHandler.font_big,"Updates availiable")
